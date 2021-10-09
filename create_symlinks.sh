4ba9bb6482b8015a97dc6c58f0b13afcea3bc0f9
Otto Quaglio <otto.q@gmx.com>
2021-10-09 10:00:03 +0800

    add gitignore

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,7 +4,9 @@ ln -sf ~/.dotfiles/.aliases ~/.aliases
 ln -sf ~/.dotfiles/.bash_env ~/.bash_env
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
+ln -sf ~/.dotfiles/.gitignore ~/.gitignore
 ln -sf ~/.dotfiles/.nanorc ~/.nanorc
 ln -sf ~/.dotfiles/.nano ~/.nano
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+
 #ln -sf ~/.dotfiles/.zshrc ~/.zshrc
