26c33e76e2a3bff3b16635617148d9898716e272
Otto Quaglio <otto.q@gmx.com>
2024-03-10 11:58:11 +0800

    Update create_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # caution - will overwrite existing files
-ln -sf ~/.dotfiles/.aliases ~/.aliases
+#ln -sf ~/.dotfiles/.aliases ~/.aliases
+ln -sf ~/.dotfiles/.git_aliases ~/.git_aliases
 ln -sf ~/.dotfiles/.bash_env ~/.bash_env
 ln -sf ~/.dotfiles/.env ~/.env
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
@@ -9,5 +10,4 @@ ln -sf ~/.dotfiles/.gitignore ~/.gitignore
 ln -sf ~/.dotfiles/.nanorc ~/.nanorc
 ln -sf ~/.dotfiles/.nano ~/.nano
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-
 #ln -sf ~/.dotfiles/.zshrc ~/.zshrc
