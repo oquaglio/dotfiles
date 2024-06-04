5887a00f944c3558c435e47582d1878375ef61b4
Otto Quaglio <otto.q@gmx.com>
2024-06-04 16:47:29 +0800

    Update create_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # caution - will overwrite existing files
-ln -sf ~/.dotfiles/.aliases ~/.aliases
-ln -sf ~/.dotfiles/.bash_env ~/.bash_env
-ln -sf ~/.dotfiles/.env ~/.env
-ln -sf ~/.dotfiles/.gitconfig.custom ~/.gitconfig.custom
-ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
-ln -sf ~/.dotfiles/.gitignore ~/.gitignore
-ln -sf ~/.dotfiles/.git_aliases ~/.git_aliases
+#ln -sf ~/.dotfiles/.aliases ~/.aliases
+#ln -sf ~/.dotfiles/.bash_env ~/.bash_env
+#ln -sf ~/.dotfiles/.env ~/.env
+#ln -sf ~/.dotfiles/.gitconfig.custom ~/.gitconfig.custom
+#ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
+#ln -sf ~/.dotfiles/.gitignore ~/.gitignore
+#ln -sf ~/.dotfiles/.git_aliases ~/.git_aliases
 ln -sf ~/.dotfiles/.nanorc ~/.nanorc
 ln -sf ~/.dotfiles/.nano ~/.nano
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-#ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 
 # create file for local config (not commited)
-touch ~/.env.local
+#touch ~/.env.local
