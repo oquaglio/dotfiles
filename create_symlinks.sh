81f49248d849d06188434af141df4d85bf7cb9ff
Otto Quaglio <otto.q@gmx.com>
2024-08-23 17:18:30 +0800

    Update create_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -4,7 +4,7 @@ ln -sf ~/.dotfiles/.aliases ~/.aliases
 #ln -sf ~/.dotfiles/.bash_env ~/.bash_env
 ln -sf ~/.dotfiles/.env ~/.env
 #ln -sf ~/.dotfiles/.gitconfig.custom ~/.gitconfig.custom
-#ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
+ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
 #ln -sf ~/.dotfiles/.gitignore ~/.gitignore
 #ln -sf ~/.dotfiles/.git_aliases ~/.git_aliases
 ln -sf ~/.dotfiles/.nanorc ~/.nanorc
