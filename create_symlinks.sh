32130c0146376acb261ecb0f756ca6005f9357da
Otto Quaglio <otto.q@gmx.com>
2024-08-23 21:31:13 +0800

    Update create_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# caution - will overwrite existing files
-ln -sf ~/.dotfiles/.aliases ~/.aliases
-#ln -sf ~/.dotfiles/.bash_env ~/.bash_env
-ln -sf ~/.dotfiles/.env ~/.env
-#ln -sf ~/.dotfiles/.gitconfig.custom ~/.gitconfig.custom
-#ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
-#ln -sf ~/.dotfiles/.gitignore ~/.gitignore
-#ln -sf ~/.dotfiles/.git_aliases ~/.git_aliases
 ln -sf ~/.dotfiles/.nanorc ~/.nanorc
 ln -sf ~/.dotfiles/.nano ~/.nano
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
 
-# create file for local config (not commited)
-#touch ~/.env.local
+#ln -sf ~/.dotfiles/.aliases ~/.aliases
+#ln -sf ~/.dotfiles/.bash_env ~/.bash_env
+#ln -sf ~/.dotfiles/.env ~/.env
+#ln -sf ~/.dotfiles/.gitconfig.custom ~/.gitconfig.custom
+#ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
+#ln -sf ~/.dotfiles/.gitignore ~/.gitignore
+#ln -sf ~/.dotfiles/.git_aliases ~/.git_aliases
