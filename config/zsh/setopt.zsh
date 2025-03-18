62c7abd35aee8bcd8ee9d00b4af68852e3b6273c
Otto Quaglio <otto.q@gmx.com>
2025-03-18 08:51:16 +0800

    add AUTO_CD for zsh

--- config/zsh/setopt.zsh
+++ config/zsh/setopt.zsh
@@ -1,2 +1,8 @@
-setopt CORRECT
-setopt CORRECT_ALL
+# cd to dir wthout cd
+setopt AUTO_CD
+
+# auto-correction for typos when changing directories or running commands
+#setopt CORRECT
+
+# Extends CORRECT to correct typos in command arguments as well, not just the command
+# setopt CORRECT_ALL
