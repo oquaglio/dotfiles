af3541b23c4da21c3d56c443c26282afdbc606bc
Otto Quaglio <otto.q@gmx.com>
2025-05-13 16:53:17 +0800

    update symlinks

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -12,8 +12,8 @@ mkdir -p ~/.config
 ln -sf $DOTFILES_ROOT/config/starship/starship.toml ~/.config/starship.toml
 ln -sf $DOTFILES_ROOT/config/nano/.nanorc ~/.nanorc
 # TODO: fix nano config
-ln -s "$DOTFILES_ROOT/config/nano/.nano" ~/.nano
-ln -s "$DOTFILES_ROOT/config/nano/.nano" ~/.config/nano
+ln -sf "$DOTFILES_ROOT/config/nano/.nano" ~/.nano
+ln -sf "$DOTFILES_ROOT/config/nano/.nano" ~/.config/nano
 
 # .gitconfig doesn't support env vars, so instead link them to $HOME
 ln -sf $DOTFILES_ROOT/config/git/.gitmessage $HOME/.gitmessage
